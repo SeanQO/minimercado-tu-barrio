91712583db9c93bbebe52801cc60a9e0e6d96a2b
SeanQO <seanquintero3@gmail.com>
2020-08-21 13:33:51 -0500

    First Commit

---
+++ src/ui/Menu.java
@@ -0,0 +1,15 @@
+package ui;
+import model.*;
+import java.util.*;
+
+public class Menu {
+	private Store store;
+	private Scanner in;
+	
+	public Menu(){
+		store = new Store();
+		in = new Scanner(System.in);
+
+	}
+	
+}
